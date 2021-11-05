e47af5ebc8267106c4b23123d2aa3f1dc757b2db
realmattclark <matt.clark5991@gmail.com>
2021-11-04 22:49:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import csv
 
+def create_reported_date_dict(data):
+    return create_dict_column(data,'Reported_Date')
+
+def create_offense_dict(data):
+    return create_dict_column(data,"Offense")
+
 def report_month_dict():
     file_list = read_in_file('CrimeDataSmall.csv')
     month_dict = create_reported_month_dict(file_list)
-    month_dict
+    return month_dict
 
 def create_reported_month_dict(data):
     dict_ex = {}
-    for sub in data[1:]:
-        temp = sub[1][0:2]
+    for i in data[1:]:
+        temp = i[1][0:2]
         if temp in dict_ex:
             dict_ex[temp]+=1
         else:
             dict_ex[temp]=1
+    return dict_ex
+
+def create_dict_column(data,name):
+    dict_ex = {}
+    n = data[0].index(name)
+    for i in data[1:]:
+        if i[n]in dict_ex:
+            dict_ex[i[n]]+=1
+        else:
+            dict_ex[i[n]]=1
     return dict_ex
 
 def read_in_file(fname):
@@ -27,56 +43,40 @@ def read_in_file(fname):
     f.close()
     return data
 
-def create_dict_column(data,name):
-    dict_ex = {}
-    n = data[0].index(name)
-    for sub in data[1:]:
-        if sub[n]in dict_ex:
-            dict_ex[sub[n]]+=1
-        else:
-            dict_ex[sub[n]]=1
-    return dict_ex
-
-def create_reported_date_dict(data):
-    return create_dict_column(data,'Reported_Date')
-
-def create_offense_dict(data):
-    return create_dict_column(data,"Offense")
-
 
 
 def create_offense_by_zip(data):
     dict_ex={}
-    for sub in data[1:]:
-        temp = sub[7]
+    for i in data[1:]:
+        temp = i[7]
         if temp in dict_ex:
             temp_dict = dict_ex[temp]
-            if sub[13] in temp_dict:
-                temp_dict[sub[13]]+=1
+            if i[13] in temp_dict:
+                temp_dict[i[13]]+=1
             else:
-                temp_dict[sub[13]] = 1
+                temp_dict[i[13]] = 1
         else:
-            dict_ex[temp] = {sub[13]:1}
+            dict_ex[temp] = {i[13]:1}
     return dict_ex
 
     
             
 
 def main():
-    while(True):
-        fname = input("Enter the name of the crime data file ==> ")
-        data = read_in_file(fname)
+    while True:
+        file_name = input("Enter the name of the crime data file > ")
+        data = read_in_file(file_name)
         if(data==-1):
-            print("Could not find the file specified. "+fname+" not found")
+            print("Could not find the file specified. ", file_name, " not found")
         else:
             break
     month = create_reported_month_dict(data)
     print(month)
-    mx = max(month,key=month.get)
-    print("The month with the highest # of crimes is "+month_from_number(mx)+" with "+str(month[mx])+" offenses")
+    month_max = max(month,key=month.get)
+    print("The month with the highest # of crimes is ", month, " with ", month_max, " offenses")
     offense = create_offense_dict(data)
-    mx = max(offense,key=offense.get)
-    print("The offense with the highest # of crimes is "+mx+" with "+str(offense[mx])+" offenses")
+    crime_max = max(offense,key=offense.get)
+    print("The offense with the highest # of crimes is ", create_offense_dict, " with ", crime_max, " offenses")
     offense = create_offense_by_zip(data)
     print()
     while(True):
@@ -87,8 +87,8 @@ def main():
             break
     print(offense, " offense by Zip Code")
     print("Zip Code\t\t\t# Offenses")
-    for k,v in offense[off].items():
-        print(k+"\t\t\t\t\t"+str(v))
+    for k,v in offense.items():
+        print(k, "\t\t\t\t\t", v.str())
 
 if __name__ == "__main__":
     main()
